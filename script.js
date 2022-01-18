940a8b8069c80c835709e41ab5acbe7c7fb66e86
Paola Reyes <pao_jb_mc_@hotmail.com>
2022-01-17 22:34:01 -0500

    work and footer

--- script.js
+++ script.js
@@ -2,16 +2,22 @@ $(document).ready(function(){
   let i = document.getElementById("illustrations");
   let a = document.getElementById("about");
   let w = document.getElementById("work");
+  let f = document.getElementById("footer");
 
   $("#illustration-button").click(function(){
     if (i.style.display === "none" || i.style.display === "") {
+      f.style.display = "none";
       i.style.display = "block";
       $("#illustration-button").addClass("active-box");
       if(a.style.display === "block") {
         a.style.display = "none";
         $("#about-button").removeClass("active-box");
+      } else if(w.style.display === "block"){
+        w.style.display = "none";
+        $("#work-button").removeClass("active-box");
       }
     } else {
+      f.style.display = "block";
       i.style.display = "none";
       $("#illustration-button").removeClass("active-box");
     }
@@ -23,11 +29,35 @@ $(document).ready(function(){
       $("#about-button").addClass("active-box");
       if(i.style.display === "block") {
         i.style.display = "none";
+        f.style.display = "block";
         $("#illustration-button").removeClass("active-box");
+      } else if (w.style.display === "block") {
+        w.style.display = "none";
+        f.style.display = "block";
+        $("#work-button").removeClass("active-box");
       }
     } else {
       a.style.display = "none";
       $("#about-button").removeClass("active-box");
+    }
+  });
+
+  $("#work-button").click(function(){
+    if (w.style.display === "none" || w.style.display === "") {
+      w.style.display = "block";
+      f.style.display = "none";
+      $("#work-button").addClass("active-box");
+      if(i.style.display === "block" ) {
+        i.style.display = "none";
+        $("#illustration-button").removeClass("active-box");
+      } else if(a.style.display === "block"){
+        a.style.display = "none";
+        $("#about-button").removeClass("active-box");
+      }
+    } else {
+      w.style.display = "none";
+      f.style.display = "block";
+      $("#work-button").removeClass("active-box");
     }
   });
 
@@ -38,33 +68,56 @@ function closeAbout(){
   $("#about-button").removeClass("active-box");
 }
 
-function openModal() {
-  document.getElementById('myModal').style.display = "block";
+function openModal(name) {
+  if(name === "work"){
+    document.getElementById('workModal').style.display = "block";
+  } else{
+    document.getElementById('myModal').style.display = "block";
+  }
 }
 
-function closeModal() {
-  document.getElementById('myModal').style.display = "none";
+function closeModal(name) {
+  if(name === "work"){
+    document.getElementById('workModal').style.display = "none";
+  } else{
+    document.getElementById('myModal').style.display = "none";
+  }
 }
 
 var slideIndex = 1;
 showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n, name) {
+  if(name === 'work'){
+    showSlides(slideIndex += n, 'work');
+  }else {
+    showSlides(slideIndex += n);
+  }
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function currentSlide(n, name) {
+  if(name === 'work'){
+    showSlides(slideIndex = n, 'work');
+  }else{
+    showSlides(slideIndex = n);
+  }
 }
 
-function showSlides(n) {
+function showSlides(n, name) {
   var i;
-  var slides = document.getElementsByClassName("item-slide");
-  var captionText = document.getElementById("caption");
+  if(name === 'work'){
+    var slides = document.getElementsByClassName("work-slide");
+    var captionText = document.getElementById("work-caption");
+  } else {
+    var slides = document.getElementsByClassName("item-slide");
+    var captionText = document.getElementById("caption");
+  }
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
+
   slides[slideIndex-1].style.display = "block";
+
 }
